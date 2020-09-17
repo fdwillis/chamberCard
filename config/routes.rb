7b236ff7f38e0d54f8733731d8d7cafd15b15cd7
pushdisinc <fdwillis7+pushdis@gmail.com>
2020-09-17 11:51:58 -0500

    first ui on login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	get 'auth/_manifest.json' => 'home#manifest'
   devise_for :users, path: '/', path_names: { sign_in: 'auth/login', sign_out: 'auth/logout', sign_up: 'auth/sign-up' }, controllers: { registrations: 'registrations', sessions: 'sessions'}
 	root to: "home#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
