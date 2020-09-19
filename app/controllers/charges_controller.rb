a814f391cc6cf289bad3f4e731448a31b1f4e988
pushdisinc <fdwillis7+pushdis@gmail.com>
2020-09-18 23:39:29 -0500

    make buttons work, add flow for user connecting stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,9 +1,10 @@
 class ChargesController < ApplicationController
 	def index
 		if current_user.present?
-			curlCall = `curl -d "" -X GET #{SITEurl}/v1/stripe-charges`
+			curlCall = `curl -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -d "" -X GET #{SITEurl}/v1/stripe-charges`
 
 	    response = Oj.load(curlCall)
+	    
 	    if !response.blank? && response['success']
 				@payments = response['payments']
 				@overdue = response['overdue']
