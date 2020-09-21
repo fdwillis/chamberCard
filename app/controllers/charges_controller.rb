54b8de27fdad2165ebd887c59f6f9d51ece3578c
pushdisinc <fdwillis7+pushdis@gmail.com>
2020-09-21 17:13:15 -0500

    link buttons book now, counter, and request schedule

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -28,7 +28,7 @@ class ChargesController < ApplicationController
 		desc = params[:newCharge][:desc]
     
     curlCall = `curl -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -d "quantity=#{quantity}&timeSlot=#{timeSlot}&timeSlotCharge=true&description=#{desc}" #{SITEurl}/v1/stripe-charges`
-		debugger
+
 		response = Oj.load(curlCall)
     
     if !response.blank? && response['success']
