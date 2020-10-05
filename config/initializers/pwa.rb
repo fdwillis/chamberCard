b34bd1b55c0a008a8f05fe3158e77fe6d6cbf265
pushdisinc <fdwillis7+pushdis@gmail.com>
2020-10-05 14:47:22 -0500

    pwa

--- config/initializers/pwa.rb
+++ config/initializers/pwa.rb
@@ -4,5 +4,6 @@ Pwa.configure do |config|
   # Define Progressive Web Apps for Rails apps
   # config.define_app 'Subdomain', ['subdomain.example.com',
   #                                 'subdomain.lvh.me:3000']
-  config.define_app 'Messy Lawns', ['http://localhost:3001']
+  config.define_app 'Messy Lawns', ['http://localhost:3001',
+  																	'https://dry-beach-36435.herokuapp.com']
 end
