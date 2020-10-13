61f37dd61eee3d29428e2203e71c5c3dd0092312
pushdisinc <fdwillis7+pushdis@gmail.com>
2020-10-13 09:44:35 -0500

    address to request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,27 +1,29 @@
 class HomeController < ApplicationController
 
 	def profile
-		if current_user&.stripeUserID
-			callCurl = current_user.showStripeUserAPI
+		if current_user
+			if current_user&.stripeUserID
+				callCurl = current_user.showStripeUserAPI
 
-			if callCurl['success']
-				@sources = callCurl['sources']
+				if callCurl['success']
+					@sources = callCurl['sources']
 
-				if !current_user.manager?
-					@phone = callCurl['stripeCustomer']['phone']
-					@name = callCurl['stripeCustomer']['name']
-					@email = callCurl['stripeCustomer']['email']
+					if !current_user.manager?
+						@phone = callCurl['stripeCustomer']['phone']
+						@name = callCurl['stripeCustomer']['name']
+						@email = callCurl['stripeCustomer']['email']
 
-				else
-					
-					if callCurl['stripeCustomer']['capabilities']["card_payments"] == "inactive" &&
-						 callCurl['stripeCustomer']['capabilities']["transfers"] == "inactive"
-						@pending = true
-					end
+					else
+						
+						if callCurl['stripeCustomer']['capabilities']["card_payments"] == "inactive" &&
+							 callCurl['stripeCustomer']['capabilities']["transfers"] == "inactive"
+							@pending = true
+						end
 
-					if callCurl['stripeCustomer']['individual']['phone']
-						@phone = callCurl['stripeCustomer']['individual']['phone'][2..12]
-						@email = callCurl['stripeCustomer']['individual']['email']
+						if callCurl['stripeCustomer']['individual']['phone']
+							@phone = callCurl['stripeCustomer']['individual']['phone'][2..12]
+							@email = callCurl['stripeCustomer']['individual']['email']
+						end
 					end
 				end
 			end
