7488ecd3637cc8eb2595ee4efe37acf476f9788a
pushdisinc <fdwillis7+pushdis@gmail.com>
2021-01-17 15:05:52 -0600

    styling

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,21 +1,20 @@
 class ScheduleController < ApplicationController
 	def index
 		if current_user&.authentication_token
-			curlCall = `curl -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -d "" -X GET #{SITEurl}/v1/available-times`
-
+			curlCall = `curl -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -d "" -X GET #{SITEurl}/v1/charges`
+			
 	    response = Oj.load(curlCall)
-	    
-	    if !response.blank? && response['success']
-				@timeBought = response['timeBought']
-				@availableTimes = response['availableTimes']
-				@bookingRequests = response['bookingRequests']
-				@scheduledTimes = response['scheduledTime']
 				
-			elsif response['message'] == "Invalid Token"
-				flash[:alert] = "To authorize your account, logout then login again."
+	    if !response.blank? && response['success']
+				@payments = response['payments']
+			elsif response['message'] == "No purchases found"
+				@message = response['message']
 			else
-				flash[:alert] = "Trouble connecting. Try again later."
+				flash[:error] = response['message']
 			end
+		else
+			current_user = nil
+      reset_session
 		end
 	end
 
