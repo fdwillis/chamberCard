13c263cb01ef731fe062dc63939ad2e5a7c17395
pushdisinc <fdwillis7+pushdis@gmail.com>
2021-01-22 20:35:07 -0600

    back to products flow after payment for both parties. then move to webhook for stripe updating user subscriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.destroy_all
-manager = User.create!(username: 'manager2', stripeSourceVerified: true, stripeUserID: 'acct_1I9ZIUQhh3tbHkEK', email: 'm2@m.com', password: 'mmmmmmmm', uuid: SecureRandom.uuid[0..7], accessPin: 'manager')
+manager = User.create!(username: 'manager2', stripeSourceVerified: true, stripeUserID: 'acct_1I9ZIUQhh3tbHkEK', email: 'm@m.com', password: 'mmmmmmmm', uuid: SecureRandom.uuid[0..7], accessPin: 'manager')
