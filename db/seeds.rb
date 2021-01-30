04b6f11b46690eaeeef8608f12b42443254eb86a
pushdisinc <fdwillis7+pushdis@gmail.com>
2021-01-30 00:07:47 -0600

    data wipe. testing services flow to complete work ;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.destroy_all
-manager = User.create!(username: 'manager2', stripeMerchantID: 'acct_1IDJ8iQmUV7SXPrI', email: 'm@m.com', password: 'mmmmmmmm', uuid: SecureRandom.uuid[0..7], accessPin: 'manager')
+manager = User.create!(username: 'manager', stripeMerchantID: 'acct_1IFBXBQYnr9HT5Wc', email: 'm@m.com', password: 'mmmmmmmm', uuid: SecureRandom.uuid[0..7], accessPin: 'manager')
