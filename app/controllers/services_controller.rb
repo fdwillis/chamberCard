ee92a710194c52c637c36f0c31ce0ce78189707e
pushdisinc <fdwillis7+pushdis@gmail.com>
2021-02-03 20:54:09 -0600

    refactored services update. share with products?

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -40,7 +40,7 @@ class ServicesController < ApplicationController
 
 	def show
 		if current_user&.authentication_token
-			curlCall = `curl -H "appName: #{ENV['appName']}" -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -X GET #{SITEurl}/v1/products/prod_#{params[:id]}?connectAccount=#{params[:connectAccount]}`
+			curlCall = Service.APIshow(current_user, params)
 		
 			response = Oj.load(curlCall)
 			
@@ -61,31 +61,12 @@ class ServicesController < ApplicationController
 
 	def create
 		if current_user&.manager?
-			appName = ENV['appName']
-			productName = productParams[:name]
-			description = productParams[:description]
-			type = productParams[:type]
-			connectAccount = ENV['connectAccount']
-			keywords = productParams[:keywords]
 
-			images = []
-			productStarted = Product.create()
-			if !productParams['images'].blank?
-				productParams['images'].each do |img|
-					imageMade = productStarted.images.create(source: img)
-					cloudX = Cloudinary::Uploader.upload(imageMade.source.file.file)
-					images.append(cloudX['secure_url'])
-					File.delete(imageMade.source.file.file)
-				end
-			end
-			
+			curlCall = Service.APIcreate(current_user, productParams)
 
-			curlCall = `curl -H "appName: #{ENV['appName']}" -d 'keywords=#{keywords}&images=#{images.join(",")}&type=#{type}&name=#{productName}&description=#{description}&connectAccount=#{connectAccount}&active=true' -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -X POST #{SITEurl}/v1/products`
 			response = Oj.load(curlCall)
-			
-		
+
 			if response['success']
-				productStarted.update(stripeProductID: response['product'])
 				flash[:success] = "Service Created"
 				redirect_to services_path
 			else
@@ -97,41 +78,41 @@ class ServicesController < ApplicationController
 	end
 
 	def update
-		appName = ENV['appName']
-		productName = productParams[:name]
-		description = productParams[:description]
-		type = productParams[:type]
-		keywords = productParams[:keywords]
-		active = ActiveModel::Type::Boolean.new.cast(productParams[:active])
-		connectAccount = ENV['connectAccount']
+		# appName = ENV['appName']
+		# productName = productParams[:name]
+		# description = productParams[:description]
+		# type = productParams[:type]
+		# keywords = productParams[:keywords]
+		# active = ActiveModel::Type::Boolean.new.cast(productParams[:active])
+		# connectAccount = ENV['connectAccount']
 
-		images = []
+		# images = []
 
 
-		if !productParams['images'].blank?
-			productParams['images'].each do |img|
-				productFound = Product.find_by(stripeProductID: params[:id])
-				imageMade = productFound.images.create(source: img)
+		# if !productParams['images'].blank?
+		# 	productParams['images'].each do |img|
+		# 		productFound = Product.find_by(stripeProductID: params[:id])
+		# 		imageMade = productFound.images.create(source: img)
 				
-				cloudX = Cloudinary::Uploader.upload(imageMade.source.file.file)
-				images.append(cloudX['secure_url'])
-				File.delete(imageMade.source.file.file)
+		# 		cloudX = Cloudinary::Uploader.upload(imageMade.source.file.file)
+		# 		images.append(cloudX['secure_url'])
+		# 		File.delete(imageMade.source.file.file)
+		# 	end
+		# end
+
+
+		if current_user&.manager?
+			curlCall = Service.APIupdate(current_user, productParams)
+			response = Oj.load(curlCall)
+
+			if response['success']
+				
+				flash[:success] = "Service Updated"
+				redirect_to service_path(id: params[:id][5..params[:id].length], connectAccount: current_user&.stripeMerchantID)
+			else
+				flash[:alert] = "Trouble connecting. Try again."
+				redirect_to request.referrer
 			end
-		end
-
-
-
-		curlCall = `curl -H "appName: #{ENV['appName']}" -d "images=#{images.join(",")}&keywords=#{keywords}&name=#{productName}&description=#{description}&active=#{active}&type=#{type}&connectAccount=#{connectAccount}" -H "bxxkxmxppAuthtoken: #{current_user.authentication_token}" -X PATCH #{SITEurl}/v1/products/#{params[:id][5.. params[:id].length]}`
-		
-		response = Oj.load(curlCall)
-
-		if response['success']
-			
-			flash[:success] = "Service Updated"
-			redirect_to service_path(id: params[:id][5..params[:id].length], connectAccount: connectAccount)
-		else
-			flash[:alert] = "Trouble connecting. Try again."
-			redirect_to request.referrer
 		end
 	end
 
@@ -164,7 +145,7 @@ class ServicesController < ApplicationController
 	private
 
 	def productParams
-		paramsClean = params.require(:product).permit(:name, :description, :type, :active, {images: []}, :keywords)
+		paramsClean = params.require(:product).permit(:id, :name, :description, :type, :active, {images: []}, :keywords, :connectAccount)
 		return paramsClean.reject{|_, v| v.blank?}
 	end
 end
