7a9571d5775732d9de0d0599166fabe2cc97ee4b
Fred Willis <fdwillis7@gmail.com>
2021-03-26 22:50:09 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ Product.destroy_all
 Image.destroy_all
 User.destroy_all
 manager = User.create!(timeKitID: "7e2c060a-bcfa-4e29-a8e0-8fe7e0e1a4db",username: 'manager',stripeCustomerID: 'cus_JBeja3MqqIeuBl', stripeMerchantID: 'acct_1IZHNUQVqSB7BoLG', email: 'm@m.com', password: 'mmmmmmmm', uuid: SecureRandom.uuid[0..7], accessPin: 'manager')
+
