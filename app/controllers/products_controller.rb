52ad6ad93975e2540b94d0ad51b5753521c39af6
Fred Willis <fdwillis7@gmail.com>
2021-05-02 19:12:21 -0700

    cleaning up product side to match new flow

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-	before_action :authenticate_user!, except: :index
+	before_action :authenticate_user!, only: [:create, :update, :edit, :new]
 
 	def index
 		if current_user&.authentication_token
@@ -41,24 +41,18 @@ class ProductsController < ApplicationController
 	end
 
 	def show
-		if current_user&.authentication_token
-			curlCall = Product.APIshow(current_user, params)
+		curlCall = Product.APIshow(params)
+	
+		response = Oj.load(curlCall)
 		
-			response = Oj.load(curlCall)
-			
-			if !response['product'].blank? && response['success']
-				@product = response['product']
-				@connectAccount = response['connectAccount']
-				@prices = response['prices']
-			else
-				flash[:alert] = "Trouble connecting..."
-				redirect_to products_path
-			end
+		if !response['product'].blank? && response['success']
+			@product = response['product']
+			@connectAccount = response['connectAccount']
+			@prices = response['prices']
 		else
-			redirect_to new_user_session_path
-			flash[:alert] = 'Please login'
+			flash[:alert] = "Trouble connecting..."
+			redirect_to products_path
 		end
-
 	end
 
 	def create
