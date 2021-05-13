b61f8f085d6ab6bfb65b6e12b44023f3ad260753
Fred Willis <fdwillis7@gmail.com>
2021-05-12 23:12:53 -0700

    test flow. find expensive queries. continue testing for anon and auth users. style success page. wipe cart_id after success? send with receipet_email and edit payment intent for application_amount instead of direct charge

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -4,31 +4,44 @@ class ScheduleController < ApplicationController
 	protect_from_forgery with: :null_session, only: :timeKitCancel
 
 	def create
-		sessionOrInvoiceID = scheduleServiceParams['invoiceScheduled']
+
+		paramsX = scheduleServiceParams.to_json
+		curlCall = `curl -H "Content-Type: application/json" -H "appName: #{ENV['appName']}" -H "bxxkxmxppAuthtoken: #{current_user&.authentication_token}" -d '#{paramsX}' -X POST #{SITEurl}/v1/schedules`
+		response = Oj.load(curlCall)
+
+    if response['success']
+			flash[:success] = "Service Confirmed"
+			
+      redirect_to request.referrer
+    else
+			flash[:error] = response['message']
+      redirect_to request.referrer
+    end
 	end
 	
 	def index
 		if current_user&.authentication_token
+				chargesNcustomers
 
 			if !session[:invoices].blank?
 				@invoices = session[:invoices]
 				@pending = session[:pending]
 				@customerCharges = session[:customerCharges] #edit lineItems meta for scheduling
+				
+				if session[:charges].blank?
+					anonCharges = []
 
-				anonCharges = []
-
-				session[:charges].each do |anon|
-					
-					if anon['lineItems'].map{|litm| Stripe::Product.retrieve(litm['price']['product'], {stripe_account: ENV['connectAccount']})['type'] }.include?("service")
-						anonCharges << anon
+					session[:charges].each do |anon|
+						
+						if anon['lineItems'].map{|litm| Stripe::Product.retrieve(litm['price']['product'], {stripe_account: ENV['connectAccount']})['type'] }.include?("service")
+							anonCharges << anon
+						end
 					end
+					session[:charges] = anonCharges
+					@anonCharges = session[:charges] #edit stripe session meta for scheduling
+				else
+					@anonCharges = session[:charges] #edit stripe session meta for scheduling
 				end
-				@anonCharges = anonCharges #edit stripe session meta for scheduling
-			else
-				chargesNcustomers
-				@invoices = session[:invoices]
-				@anonCharges = session[:charges] #edit stripe session meta for scheduling
-				@customerCharges = session[:customerCharges] #edit lineItems meta for scheduling
 			end
 		else
 			current_user = nil
@@ -79,7 +92,7 @@ class ScheduleController < ApplicationController
 	private
 
 	def scheduleServiceParams
-		paramsClean = params.require(:scheduleService).permit(:invoiceScheduled)
+		paramsClean = params.require(:scheduleService).permit(:sessionOrInvoiceID)
 		return paramsClean.reject{|_, v| v.blank?}
 	end
 end
