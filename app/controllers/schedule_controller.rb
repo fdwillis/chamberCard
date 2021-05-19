d6b48eb5a15d4347ea6ecd853c16aa3d3021b63b
Fred Willis <fdwillis7@gmail.com>
2021-05-19 04:15:29 -0700

    orders and schedule ready for restyle. fix payments next

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -21,28 +21,8 @@ class ScheduleController < ApplicationController
 	
 	def index
 		if current_user&.authentication_token
-				chargesNcustomers
-
-			if !session[:invoices].blank?
-				@invoices = session[:invoices]
-				@pending = session[:pending]
-				@customerCharges = session[:customerCharges] #edit lineItems meta for scheduling
-				
-				if session[:charges].blank?
-					anonCharges = []
-
-					session[:charges].each do |anon|
-						
-						if anon['lineItems'].map{|litm| Stripe::Product.retrieve(litm['price']['product'], {stripe_account: ENV['connectAccount']})['type'] }.include?("service")
-							anonCharges << anon
-						end
-					end
-					session[:charges] = anonCharges
-					@anonCharges = session[:charges] #edit stripe session meta for scheduling
-				else
-					@anonCharges = session[:charges] #edit stripe session meta for scheduling
-				end
-			end
+			chargesNcustomers
+			@actualCharges = session[:actualCharges] #edit stripe session meta for scheduling
 		else
 			current_user = nil
       reset_session
