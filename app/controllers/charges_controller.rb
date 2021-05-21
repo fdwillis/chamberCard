2264daaf8dfc2214f1597c03cff78989e8574134
Fred Willis <fdwillis7@gmail.com>
2021-05-20 18:09:30 -0700

    styles in. test flow

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -109,6 +109,7 @@ class ChargesController < ApplicationController
 			
 	    if response['success']
 				@pending = response['pending']
+				debugger
 	    else
 				flash[:error] = response['message']
 	      redirect_to charges_path
