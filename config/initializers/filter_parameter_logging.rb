f5bc6d06cca8bc83dbc7bd9c7d8ebcbf1ed04d8d
Fred Willis <fdwillis7@gmail.com>
2021-05-29 01:47:51 -0700

    anon param filter

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:stripeCustomerID, :stripeMerchantID, :password, :carts, :connectAccount, :id, :phone, :uuid, :accessPin]
+Rails.application.config.filter_parameters += [:stripeCustomerID, :stripeMerchantID, :password, :carts, :connectAccount, :id, :phone, :uuid, :accessPin, :card, :cvc]
