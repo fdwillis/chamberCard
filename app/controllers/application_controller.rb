c4fc9242df0fb0f6678457f59a3d1b66c1446899
Fred Willis <fdwillis7@gmail.com>
2021-06-28 21:58:05 -0700

    ready to hand anonymous invoices over to api ;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,95 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	def stripeCheckoutRequest(lineItems,connectAccountCus)
+		lineItems.each do |lineItem|
+			stripePriceInfo = Stripe::Price.retrieve(lineItem[:price], {stripe_account: ENV['connectAccount']})
+			stripeProductInfo = Stripe::Product.retrieve(stripePriceInfo[:product], {stripe_account: ENV['connectAccount']})
+
+			if !stripeProductInfo.shippable
+				inI = Stripe::InvoiceItem.create({
+					currency: 'usd',
+				  customer: connectAccountCus['id'],
+				  description: stripeProductInfo[:name],
+				  unit_amount_decimal: stripePriceInfo[:unit_amount_decimal],
+				  quantity: lineItem[:quantity],
+				  metadata: {
+				  	price: stripePriceInfo[:id]
+				  }
+				}, {stripe_account: ENV['connectAccount']})
+
+
+			else
+				inI = Stripe::InvoiceItem.create({
+					currency: 'usd',
+				  customer: connectAccountCus['id'],
+				  description: stripeProductInfo[:name],
+				  unit_amount_decimal: stripePriceInfo[:unit_amount_decimal],
+				  quantity: lineItem[:quantity],
+				  metadata: {
+				  	shipping: "true",
+				  	pickup: "",
+				  	price:  stripePriceInfo[:id]
+				  }
+				}, {stripe_account: ENV['connectAccount']})
+			end
+			# make one invoice with all line items? in v2 here
+			appFeeAmount = ((stripePriceInfo[:unit_amount_decimal].to_i * lineItem[:quantity].to_i) * (ENV['serviceFee'].to_i * 0.01) ).to_i
+			
+			if session[:coupon]
+				listInvoice = Stripe::Invoice.create({
+					customer: connectAccountCus['id'],
+					application_fee_amount: (appFeeAmount * (1 - (session[:percentOff] * 0.01))).to_i,
+					discounts: [
+						{coupon: session[:coupon]},
+					],
+					metadata: {
+						goodOrService: stripeProductInfo.shippable == true ? 'good' : 'service',
+						trackingNumbers: "",
+				  	orderIssue: '',
+				  	issueResolved: '',
+					}
+				}, {stripe_account: ENV['connectAccount']})
+			else
+				listInvoice = Stripe::Invoice.create({
+					customer: connectAccountCus['id'],
+					application_fee_amount: appFeeAmount,
+					discounts: [
+						{coupon: session[:coupon]},
+					],
+					metadata: {
+						goodOrService: stripeProductInfo.shippable == true ? 'good' : 'service',
+						trackingNumbers: "",
+				  	orderIssue: '',
+				  	issueResolved: '',
+					}
+				}, {stripe_account: ENV['connectAccount']})
+			end
+
+			openInvoice = Stripe::Invoice.finalize_invoice(listInvoice[:id], {}, {stripe_account: ENV['connectAccount']})
+			payInvoice  = Stripe::Invoice.pay(listInvoice[:id], {}, {stripe_account: ENV['connectAccount']})
+		  # email customer with receipt_email update via payment intent
+		  # text owner with invoice id and ENV['connectAccount']
+		  receiptEmailX = Stripe::PaymentIntent.update(
+			  payInvoice['payment_intent'],{
+			  	receipt_email: connectAccountCus['email'],
+			  },
+			  {stripe_account: ENV['connectAccount']},
+			)
+			
+		  if ENV['stripeLivePublish'].include?("pk_live_")
+			  textData = {
+					'stripeMerchantID' => ENV['connectAccount'],
+					'stripePaymentIntentID' => payInvoice['payment_intent'],
+				}.to_json
+
+			  notifyTwilio = `curl -H "Content-Type: application/json" -H "appName: #{ENV['appName']}" -d '#{textData}' -X POST #{SITEurl}/v1/twilioText`
+	
+		    response = Oj.load(notifyTwilio)
+			end
+		end
+	end
+
 	def stripeCustomerRequest(session, token)
 		connectAccountCus = Stripe::Customer.create({
 			email: session[:email],
@@ -19,9 +108,9 @@ class ApplicationController < ActionController::Base
     cvc = newStripeCardTokenParams[:cvc]
 
     if ENV['connectAccount'].present?
-	    curlCall = `curl -H "appName: #{ENV['appName']}" -d "connectAccount=#{ENV['connectAccount']}&number=#{number}&exp_month=#{exp_month}&exp_year=#{exp_year}&cvc=#{cvc}" #{SITEurl}/v2/stripe-tokens`
+	    curlCall = `curl -H "appName: #{ENV['appName']}" -d "connectAccount=#{ENV['connectAccount']}&number=#{number}&exp_month=#{exp_month}&exp_year=#{exp_year}&cvc=#{cvc}" #{SITEurl}/v2/tokens`
 	  else
-	    curlCall = `curl -H "appName: #{ENV['appName']}" -d "number=#{number}&exp_month=#{exp_month}&exp_year=#{exp_year}&cvc=#{cvc}" #{SITEurl}/v2/stripe-tokens`
+	    curlCall = `curl -H "appName: #{ENV['appName']}" -d "number=#{number}&exp_month=#{exp_month}&exp_year=#{exp_year}&cvc=#{cvc}" #{SITEurl}/v2/tokens`
 	  end
 
     response = Oj.load(curlCall)
