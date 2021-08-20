5a90e05121ccd6b5c51f1da953afb877dab0ec16
Fred Willis <team@tewcode.com>
2021-08-20 12:30:51 -0700

    stripe amounts

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -80,7 +80,7 @@ class ChargesController < ApplicationController
 
     subtotal = stripeAmount(newInvoiceParams[:amount])
 		application_fee_amount = (subtotal * (ENV['serviceFee'].to_i * 0.01)).to_i
-		stripeFee = (((subtotal+application_fee_amount) * 0.029) + 29).to_i
+		stripeFee = (((subtotal+application_fee_amount) * 0.029) + 30).to_i
 
 		amount = subtotal + application_fee_amount + stripeFee
 
