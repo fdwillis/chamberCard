7bd36c4af4e709ec5df89a9b3c09e184abcf33e3
Fred W <fwillis@ioufinancial.com>
2022-10-07 13:31:55 -0500

    staging test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,8 @@ class RegistrationsController < ApplicationController
       curlCall = User.pipeline(params['newRegistration'])
 
       if curlCall['success']
-        newUser = User.create(userParams(curlCall['user']).merge({'password'=> params['newRegistration']['password']}))
+        newUser = User.create(userParams(curlCall['user']).merge({'password'=> params['newRegistration']['password'],
+                                                                  'phone' => params['newRegistration']['phone_number']}))
         sign_in(:user, newUser)
         flash[:success] = "Your Card Has Been Ordered! \n Welcome To Netwerth!"
         redirect_to profile_path
@@ -31,7 +32,7 @@ class RegistrationsController < ApplicationController
   private
 
   def userParams(dataX)
-    paramsClean = dataX.slice('stripeCustomerID', 'phone', 'accessPin', 'twilioPhoneVerify', 'referredBy', 'authentication_token', 'uuid', 'email', 'authentication_token')
+    paramsClean = dataX.slice('stripeCustomerID', 'phone_number', 'accessPin', 'twilioPhoneVerify', 'referredBy', 'authentication_token', 'uuid', 'email', 'authentication_token')
     return paramsClean.reject{|_, v| v.blank?}
   end
 end
