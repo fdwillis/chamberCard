60dca5dd22413cb755ba3e4927b6194a0c9b0785
Fred Willis <fdwillis7@icloud.com>
2022-12-02 07:03:36 -0500

    reject refunded

--- app/controllers/stripe_payouts_controller.rb
+++ app/controllers/stripe_payouts_controller.rb
@@ -21,7 +21,7 @@ class StripePayoutsController < ApplicationController
 
 		validateTopUps = []
 		investedAmountRunning = 0
-		@validPaymentIntents = Stripe::PaymentIntent.list({limit: 100, created: {lte: @endDate.to_time.to_i, gte: @startDate.to_time.to_i}})['data']
+		@validPaymentIntents = Stripe::PaymentIntent.list({limit: 100, created: {lte: @endDate.to_time.to_i, gte: @startDate.to_time.to_i}})['data'].reject{|e| e['refunded'] == 'true'}
 
 		@validPaymentIntents.each do |payint|
 			if !payint['metadata'].blank? && payint['metadata']['percentToInvest'].to_i > 0 
