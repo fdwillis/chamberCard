d836b6bcb3b5953ca99d4a691c6a64ddbed0b7ae
Fred Willis <fdwillis7@icloud.com>
2022-12-02 07:09:23 -0500

    reject refunded

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < Devise::SessionsController
     investedAmountRunning = 0
     validPaymentIntents = Stripe::PaymentIntent.list()['data'].map{|d| (!d['metadata']['percentToInvest'].blank?) ? (pullPayouts.append(d)) : next}
 
-    pullPayouts.reject{|e| e['refunded'] == 'true'}.each do |payint|
+    pullPayouts.reject{|e| e['refunded'] == true}.each do |payint|
       if !payint['metadata'].blank? && payint['metadata']['percentToInvest'].to_i > 0 
         amountForDeposit = payint['amount'] - (payint['amount']*0.029).to_i + 30
         investedAmount = amountForDeposit * (payint['metadata']['percentToInvest'].to_i * 0.01)
