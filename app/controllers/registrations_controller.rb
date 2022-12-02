22417adae7f5f84bf0def3edbbdfe633abd35f26
Fred Willis <fdwillis7@icloud.com>
2022-12-02 09:23:56 -0500

    remove non captured payment intents

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < ApplicationController
     investedAmountRunning = 0
     validPaymentIntents = Stripe::PaymentIntent.list(limit: 100)['data'].map{|d| (!d['metadata']['percentToInvest'].blank?) ? (pullPayouts.append(d)) : next}
 
-    pullPayouts.reject{|e| e['charges']['data'][0]['refunded'] == true}.each do |payint|
+    pullPayouts.reject{|e| e['charges']['data'][0]['refunded'] == true}.reject{|e| e['charges']['data'][0]['captured'] == false}.each do |payint|
       if !payint['metadata'].blank? && payint['metadata']['percentToInvest'].to_i > 0 
         amountForDeposit = payint['amount'] - (payint['amount']*0.029).to_i + 30
         investedAmount = amountForDeposit * (payint['metadata']['percentToInvest'].to_i * 0.01)
