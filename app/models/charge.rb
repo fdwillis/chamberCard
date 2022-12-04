0bfbb3e70b2569e099b3087ac8c34fba21d13808
Fred Willis <fdwillis7@icloud.com>
2022-12-03 21:46:52 -0500

    net numbers in ui, do issueprofit

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,3 +1,9 @@
 class Charge < ApplicationRecord
+
+	def self.paymentIntentNet(paymentIntent)
+		bt = Stripe::PaymentIntent.retrieve(paymentIntent)['charges']['data'][0]['balance_transaction']
+		balanceTransaction = Stripe::BalanceTransaction.retrieve(bt)
+		{net: balanceTransaction['net'], fee: balanceTransaction['fee'], amount: balanceTransaction['amount']}
+	end
 	
 end
